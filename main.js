59fa9dd5fac4335b3cffde86fa62f1b4ecd49a37
Kris Mannino <krismannino@gmail.com>
2022-03-02 21:53:23 -0500

    changed some js

--- main.js
+++ main.js
@@ -2,3 +2,9 @@ function darkFunction() {
   var element = document.body;
   element.classList.toggle("dark");
 }
+var img = document.getElementById("headShot");
+img.addEventListener("click", function(){
+  if(img.src != "images\Kris Mannino - Headshot.jpg"){
+    img.src = "images\Kris Mannino - Headshot.jpg";}
+  else{
+    img.src = "images\luna and me0.jpg";}});
