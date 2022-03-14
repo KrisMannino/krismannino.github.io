f727a9a84420dc2e8edba5e58efb4c3aac598d0a
Kris Mannino <krismannino@gmail.com>
2022-03-13 20:02:08 -0400

    fixed change img JavaScript function on home screen

--- main.js
+++ main.js
@@ -2,9 +2,26 @@ function darkFunction() {
   var element = document.body;
   element.classList.toggle("dark");
 }
-var img = document.getElementById("headShot");
-img.addEventListener("click", function(){
-  if(img.src != "images\Kris Mannino - Headshot.jpg"){
-    img.src = "images\Kris Mannino - Headshot.jpg";}
+
+
+
+var imgTracker = 'me';
+
+function changeImg(){
+  var img = document.getElementById("headShot");
+  if(imgTracker == 'me'){
+    img.src = "images/lunaAndI.jpg";
+    imgTracker = 'meLuna';
+  }
   else{
-    img.src = "images\luna and me0.jpg";}});
+    img.src = "images/KrisManninoHeadshot.jpg";
+    imgTracker = 'me';
+  }
+}
+/**img.addEventListener("click", function(){
+  
+  if(img.src = "images\Kris Mannino - Headshot.jpg"){
+    alert("hello");
+    img.src = "images\luna and me0.jpg";}
+  else{
+    img.src = "images\Kris Mannino - Headshot.jpg";}});*/
