60cb0a0524d5e5208f9950bc78d2fd3e24e977aa
Kris Mannino <krismannino@gmail.com>
2022-03-19 16:18:31 -0400

    changed switch resume view to one button

--- main.js
+++ main.js
@@ -26,10 +26,14 @@ function changeImg(){
   else{
     img.src = "images/Kris Mannino - Headshot.jpg";}});*/
     function replace() {
-      document.getElementById("resume-jpg").style.display="none";
-      document.getElementById("resume-text").style.display="block";
+      var button = document.getElementById("highlights-button");
+      if(button.innerHTML == "Highlights"){
+        button.innerHTML = "Full Resume"
+        document.getElementById("resume-jpg").style.display="none";
+        document.getElementById("resume-text").style.display="block"; 
+      }else {
+          button.innerHTML = "Highlights"
+          document.getElementById("resume-jpg").style.display="block";
+          document.getElementById("resume-text").style.display="none";
+        }
       }
-    function replaceAgain() {
-       document.getElementById("resume-jpg").style.display="block";
-       document.getElementById("resume-text").style.display="none";
-       }
